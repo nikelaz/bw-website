8b1a839be19fbb9268cf74d3e39268ed74d6d4fc
Nikola Lazarov <nikola.n.lazarov@outlook.com>
2024-12-21 14:32:42 +0200

    add gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { clsx } from 'clsx';
+import { GoogleTagManager } from '@next/third-parties/google';
 import './globals.sass';
 
 const inter = Inter({
@@ -19,6 +20,7 @@ type RootLayoutProps = Readonly<{
 
 const RootLayout = (props: RootLayoutProps) => (
   <html lang="en">
+    <GoogleTagManager gtmId="GTM-KSZF3MFV" />
     <body className={clsx(inter.className, 'bg-ash200')}>
       {props.children}
     </body>
